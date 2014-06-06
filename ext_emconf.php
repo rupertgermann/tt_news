a85983da2b129e2df17a1f13845b003fe19ee9f7
TYPO3 Extension Repository (TER) <ter@endor.wmdb.de>
2014-06-07 00:21:21 +0200

    Initial import of tt_address

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "tt_address".
+ *
+ * Auto generated 07-06-2014 00:21
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Address list',
+	'description' => 'Displays a list of addresses from an address table on the page.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '',
+	'PHP_version' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author' => 'Kasper Skårhøj',
+	'author_email' => 'kasper@typo3.com',
+	'author_company' => 'Curby Soft Multimedia',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '1.0.3',
+);
+
+?>
