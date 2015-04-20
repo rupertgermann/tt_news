116b80c3c2d5d54b7741ffe445599b4c2eda9d43
Rupert Germann <rupert.germann@wmdb.de>
2015-04-20 18:42:51 +0200

    bugfix: wrong path for flexform.xml

--- ext_tables.php
+++ ext_tables.php
@@ -114,7 +114,7 @@ TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToInsertRecords('tt_news')
 
 	// switch the XML files for the FlexForm depending on if "use StoragePid"(general record Storage Page) is set or not.
 if ($confArr['useStoragePid']) {
-	TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(9, 'FILE:EXT:tt_news/Resources/Private/Flexform/flexform_ds.xml.xml');
+	TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(9, 'FILE:EXT:tt_news/Resources/Private/Flexform/flexform_ds.xml');
 } else {
 	TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(9, 'FILE:EXT:tt_news/Resources/Private/Flexform/flexform_ds_no_sPID.xml');
 }
