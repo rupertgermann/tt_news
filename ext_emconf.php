236d9c5d53f37090d7e21de38053b23fe4a0b41b
Rupert Germann <rupert.germann@noerdisch.de>
2017-10-09 10:36:25 +0200

    [TASK] updated emconf file to test packagist service

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,6 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "tt_news".
- *
- * Auto generated 07-06-2014 00:22
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'News',
     'description' => 'Website news with front page teasers and article handling inside.',
     'category' => 'plugin',
@@ -24,11 +14,11 @@ $EM_CONF[$_EXTKEY] = array(
     'author' => 'Rupert Germann [noerdisch]',
     'author_email' => 'rupi@gmx.li',
     'author_company' => 'www.noerdisch.de',
-    'constraints' => array(
-        'depends' => array(
+    'constraints' => [
+        'depends' => [
             'typo3' => '8.7.0-8.7.99',
-        ),
-        'conflicts' => array(),
-        'suggests' => array(),
-    ),
-);
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
