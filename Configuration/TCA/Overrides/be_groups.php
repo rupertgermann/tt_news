0a8b0d360f588d27842966d4fd421524dd8c7bc6
Adrien Jacob <adrien.jacob@gmail.com>
2017-11-07 16:01:10 +0100

    [BUGFIX] Reset $tempColumns in TCA overrides
    $tempColumns is also being used by TCA definition of the
    sr_language_menu extension. When the latter is called before tt_news, it
    means $tempColumns is already filled with data relevant to
    sr_language_menu only, resulting in many issues.

--- Configuration/TCA/Overrides/be_groups.php
+++ Configuration/TCA/Overrides/be_groups.php
@@ -1,6 +1,7 @@
 <?php
 defined('TYPO3_MODE') or die();
 // Load field definition from 'shared' file
+$tempColumns = array();
 $tempColumns['tt_news_categorymounts'] = require TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('tt_news')
                                                  . 'Configuration/TCA/Shared/categorymounts.php';
 TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('be_groups', $tempColumns);
