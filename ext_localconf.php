a532e43d8611855849b07bb46d196c26be5824bc
Rupert Germann <rupert.germann@pick2.de>
2020-04-25 13:07:27 +0200

    moved global stuff from localconf and ext_tables to a function

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,24 +1,22 @@
 <?php
-if (!defined("TYPO3_MODE")) {
-    die ("Access denied.");
-}
+defined('TYPO3_MODE') or die();
 
-/**
- * Register hooks in TCEmain:
- */
+$boot = function () {
+    /**
+     * Register hooks in TCEmain:
+     */
 // this hook is used to prevent saving of news or category records which have categories assigned that are not allowed for the current BE user.
 // The list of allowed categories can be set with 'tt_news_cat.allowedItems' in user/group TSconfig.
 // This check will be disabled until 'options.useListOfAllowedItems' (user/group TSconfig) is set to a value.
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['tt_news'] =
-    \RG\TtNews\Hooks\DataHandlerHook::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['tt_news'] =
+        \RG\TtNews\Hooks\DataHandlerHook::class;
 
 // this hook is used to prevent saving of a news record that has non-allowed categories assigned when a command is executed (modify,copy,move,delete...).
 // it checks if the record has an editlock. If true, nothing will not be saved.
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass']['tt_news'] =
-    \RG\TtNews\Hooks\DataHandlerHook::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass']['tt_news'] =
+        \RG\TtNews\Hooks\DataHandlerHook::class;
 
-$confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['tt_news']);
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
   plugin.tt_news = USER
   plugin.tt_news {
     userFunc = ' . \RG\TtNews\Plugin\TtNews::class . '->main_news
@@ -29,48 +27,53 @@ TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
 ');
 
 // add default rendering for pi_layout plugin
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-    'tt_news',
-    'setup',
-    'tt_content.list.20.9 =< plugin.tt_news',
-    'defaultContentRendering'
-);
-if (TYPO3_MODE === 'BE') {
-    // Apply PageTSconfig
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tt_news/Configuration/PageTS/PageTs.ts">'
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+        'tt_news',
+        'setup',
+        'tt_content.list.20.9 =< plugin.tt_news',
+        'defaultContentRendering'
     );
+    if (TYPO3_MODE === 'BE') {
+        // Apply PageTSconfig
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+            '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tt_news/Configuration/PageTS/PageTs.ts">'
+        );
 
 // Page module hook
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['9']['tt_news'] = \RG\TtNews\Hooks\PageModuleHook::class . '->getExtensionSummary';
-}
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['9']['tt_news'] = \RG\TtNews\Hooks\PageModuleHook::class . '->getExtensionSummary';
+    }
 
 
-if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tt_news_cache'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tt_news_cache'] = array(
-        'backend' => TYPO3\CMS\Core\Cache\Backend\Typo3DatabaseBackend::class,
-        'frontend' => TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class
-    );
-}
+    if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tt_news_cache'])) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tt_news_cache'] = array(
+            'backend' => TYPO3\CMS\Core\Cache\Backend\Typo3DatabaseBackend::class,
+            'frontend' => TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class
+        );
+    }
 
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['tt_news_catmenu'] = RG\TtNews\Controller\NewsFrontendAjaxController::class . '::dispatch';
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['tt_news_catmenu'] = RG\TtNews\Controller\NewsFrontendAjaxController::class . '::dispatch';
 
 
 // register news cache table for "clear all caches"
-$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearAllCache_additionalTables']['tt_news_cache'] = 'tt_news_cache';
+    $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearAllCache_additionalTables']['tt_news_cache'] = 'tt_news_cache';
 
 
 // in order to make "direct Preview links" for tt_news work again in TYPO3 >= 6, unset pageNotFoundOnCHashError if a BE_USER is logged in
-$configuredCookieName = trim($GLOBALS['TYPO3_CONF_VARS']['BE']['cookieName']);
-if (empty($configuredCookieName)) {
-    $configuredCookieName = 'be_typo_user';
-}
-if ($_COOKIE[$configuredCookieName]) {
-    $GLOBALS['TYPO3_CONF_VARS']['FE']['pageNotFoundOnCHashError'] = 0;
-}
+    $configuredCookieName = trim($GLOBALS['TYPO3_CONF_VARS']['BE']['cookieName']);
+    if (empty($configuredCookieName)) {
+        $configuredCookieName = 'be_typo_user';
+    }
+    if ($_COOKIE[$configuredCookieName]) {
+        $GLOBALS['TYPO3_CONF_VARS']['FE']['pageNotFoundOnCHashError'] = 0;
+    }
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord'][\RG\TtNews\Form\FormDataProvider::class] = array(
-	'depends' => array(
-		\TYPO3\CMS\Backend\Form\FormDataProvider\DatabaseRowInitializeNew::class,
-	)
-);
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord'][\RG\TtNews\Form\FormDataProvider::class] = array(
+        'depends' => array(
+            \TYPO3\CMS\Backend\Form\FormDataProvider\DatabaseRowInitializeNew::class,
+        )
+    );
+
+};
+
+$boot();
+unset($boot);
--- ext_tables.php
+++ ext_tables.php
@@ -1,106 +1,111 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
-}
+defined('TYPO3_MODE') or die();
+
+$boot = function () {
 
 // get extension configuration
 $confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['tt_news']);
 
 
 // allow news and news-category records on normal pages
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tt_news_cat');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tt_news');
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tt_news_cat');
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tt_news');
 
 
 // initalize "context sensitive help" (csh)
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tt_news',
-    'EXT:tt_news/csh/locallang_csh_ttnews.php');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tt_news_cat',
-    'EXT:tt_news/csh/locallang_csh_ttnewscat.php');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('xEXT_tt_news',
-    'EXT:tt_news/csh/locallang_csh_manual.xml');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_web_txttnewsM1',
-    'EXT:tt_news/csh/locallang_csh_mod_newsadmin.xml');
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tt_news',
+        'EXT:tt_news/csh/locallang_csh_ttnews.php');
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tt_news_cat',
+        'EXT:tt_news/csh/locallang_csh_ttnewscat.php');
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('xEXT_tt_news',
+        'EXT:tt_news/csh/locallang_csh_manual.xml');
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_web_txttnewsM1',
+        'EXT:tt_news/csh/locallang_csh_mod_newsadmin.xml');
 
 
-if (TYPO3_MODE == 'BE') {
-    if ($confArr['showBackEndModule']) {
-        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
-            'web',
-            'txttnewsM1',
-            '',
-            null,
-            [
-                'routeTarget' => RG\TtNews\Module\NewsAdminModule::class . '::mainAction',
-                'access' => 'user,group',
-                'name' => 'web_txttnewsM1',
-                'navigationComponentId' => 'typo3-pagetree',
-                'icon' => 'EXT:tt_news/Classes/Module/moduleicon.gif',
+    if (TYPO3_MODE == 'BE') {
+        if ($confArr['showBackEndModule']) {
+            TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
+                'web',
+                'txttnewsM1',
+                '',
+                null,
+                [
+                    'routeTarget' => RG\TtNews\Module\NewsAdminModule::class . '::mainAction',
+                    'access' => 'user,group',
+                    'name' => 'web_txttnewsM1',
+                    'navigationComponentId' => 'typo3-pagetree',
+                    'icon' => 'EXT:tt_news/Classes/Module/moduleicon.gif',
 
-                'labels' => [
+                    'labels' => [
 
-                    'll_ref' => 'LLL:EXT:tt_news/Classes/Module/locallang_mod.xml',
-                ],
-            ]
-        );
-    }
+                        'll_ref' => 'LLL:EXT:tt_news/Classes/Module/locallang_mod.xml',
+                    ],
+                ]
+            );
+        }
 
     $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['cms']['db_layout']['addTables'][$_EXTKEY][0]['fList'] = 'uid,title,author,category,datetime,archivedate,tstamp';
     $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['cms']['db_layout']['addTables'][$_EXTKEY][0]['icon'] = true;
 
-    // Register context menu for the tt_news category manager
-    $GLOBALS['TBE_MODULES_EXT']['xMOD_alt_clickmenu']['extendCMclasses'][] = array(
-        'name' => \RG\TtNews\ClickMenu::class
-    );
+        // Register context menu for the tt_news category manager
+        $GLOBALS['TBE_MODULES_EXT']['xMOD_alt_clickmenu']['extendCMclasses'][] = array(
+            'name' => \RG\TtNews\ClickMenu::class
+        );
 
-    /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    $iconRegistry->registerIcon(
-        'apps-pagetree-folder-contains-news',
-        \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-        array(
-            'source' => 'EXT:tt_news/Resources/Public/Images/Icons/ext_icon_ttnews_folder.gif',
-        )
-    );
-    $iconRegistry->registerIcon(
-        'ttnews-content-element-wizard-icon',
-        \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-        array(
-            'source' => 'EXT:tt_news/Resources/Public/Images/ContentElementWizardIcon.gif',
-        )
-    );
+        /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
+        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+        $iconRegistry->registerIcon(
+            'apps-pagetree-folder-contains-news',
+            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+            array(
+                'source' => 'EXT:tt_news/Resources/Public/Images/Icons/ext_icon_ttnews_folder.gif',
+            )
+        );
+        $iconRegistry->registerIcon(
+            'ttnews-content-element-wizard-icon',
+            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+            array(
+                'source' => 'EXT:tt_news/Resources/Public/Images/ContentElementWizardIcon.gif',
+            )
+        );
 
-    $iconRegistry->registerIcon(
-        'tt-news',
-        \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-        array(
-            'source' => 'EXT:tt_news/Resources/Public/Images/Icons/ext_icon.gif',
-        )
-    );
-    $iconRegistry->registerIcon(
-        'tt-news-article',
-        \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-        array(
-            'source' => 'EXT:tt_news/Resources/Public/Images/Icons/tt_news_article.gif',
-        )
-    );
-    $iconRegistry->registerIcon(
-        'tt-news-exturl',
-        \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-        array(
-            'source' => 'EXT:tt_news/Resources/Public/Images/Icons/tt_news_exturl.gif',
-        )
-    );
+        $iconRegistry->registerIcon(
+            'tt-news',
+            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+            array(
+                'source' => 'EXT:tt_news/Resources/Public/Images/Icons/ext_icon.gif',
+            )
+        );
+        $iconRegistry->registerIcon(
+            'tt-news-article',
+            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+            array(
+                'source' => 'EXT:tt_news/Resources/Public/Images/Icons/tt_news_article.gif',
+            )
+        );
+        $iconRegistry->registerIcon(
+            'tt-news-exturl',
+            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+            array(
+                'source' => 'EXT:tt_news/Resources/Public/Images/Icons/tt_news_exturl.gif',
+            )
+        );
 
-    // add folder icon
-    $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-tt_news'] = 'apps-pagetree-folder-contains-news';
-    $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = array(
-        0 => 'LLL:EXT:tt_news/Resources/Private/Language/locallang_tca.xlf:tt_news',
-        1 => 'tt_news',
-        2 => 'apps-pagetree-folder-contains-news'
-    );
+        // add folder icon
+        $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-tt_news'] = 'apps-pagetree-folder-contains-news';
+        $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = array(
+            0 => 'LLL:EXT:tt_news/Resources/Private/Language/locallang_tca.xlf:tt_news',
+            1 => 'tt_news',
+            2 => 'apps-pagetree-folder-contains-news'
+        );
 
-    // Register HTML template for the tt_news BackEnd Module
-    $GLOBALS['TBE_STYLES']['htmlTemplates']['mod_ttnews_admin.html'] = TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('tt_news') . 'mod1/mod_ttnews_admin.html';
+        // Register HTML template for the tt_news BackEnd Module
+        $GLOBALS['TBE_STYLES']['htmlTemplates']['mod_ttnews_admin.html'] = TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('tt_news') . 'mod1/mod_ttnews_admin.html';
 
-}
+    }
+};
+
+$boot();
+unset($boot);
+
