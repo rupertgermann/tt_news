c42fd7da69ec0cb578299381f129c6723abaa1d9
Rupert Germann <rupert.germann@pick2.de>
2020-05-02 09:37:26 +0200

    added an upgradeWizard to migrate tt_news images to FAL

---
+++ Classes/Updates/migrateImagesToFal.php
@@ -0,0 +1,255 @@
+<?php
+
+namespace RG\TtNews\Updates;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2005-2020 Rupert Germann <rupi@gmx.li>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use Doctrine\DBAL\DBALException;
+use InvalidArgumentException;
+use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\ReferenceIndex;
+use TYPO3\CMS\Core\Resource\DuplicationBehavior;
+use TYPO3\CMS\Core\Resource\File;
+use TYPO3\CMS\Core\Resource\Folder;
+use TYPO3\CMS\Core\Resource\ResourceFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Install\Updates\DatabaseUpdatedPrerequisite;
+use TYPO3\CMS\Install\Updates\UpgradeWizardInterface;
+
+/**
+ * Migrates tt_news images from uploads/pics to FAL
+ *
+ */
+class migrateImagesToFal implements UpgradeWizardInterface
+{
+    /**
+     * @var string
+     */
+    protected $table = 'tt_news';
+    /**
+     * @var ConnectionPool
+     */
+    protected $connectionPool;
+
+    /**
+     * @return string Unique identifier of this updater
+     */
+    public function getIdentifier(): string
+    {
+        return 'migrateImagesToFal';
+    }
+
+    /**
+     * @return string Title of this updater
+     */
+    public function getTitle(): string
+    {
+        return '[tt_news images to FAL] Migrates tt_news images from uploads/pics to FAL';
+    }
+
+    /**
+     * @return string Longer description of this updater
+     */
+    public function getDescription(): string
+    {
+        return 'Moves tt_news images from uploads/pics to fileadmin/migratedNewsAssets/Images and registers them in FAL. 
+                         ******    CAUTION!!!    ******
+Make sure you have made a backup of your database and of the "uploads/pic" folder! 
+The database and filesystem operations are destructive, there is no rollback! 
+You have been warned ;-)';
+    }
+
+    /**
+     * Checks whether updates are required.
+     *
+     * @return bool Whether an update is required (TRUE) or not (FALSE)
+     * @throws DBALException
+     */
+    public function updateNecessary(): bool
+    {
+        $this->connectionPool = GeneralUtility::makeInstance(ConnectionPool::class);
+
+        $updateNeeded = false;
+        // Check if the database table even exists
+        if ($this->checkIfWizardIsRequired()) {
+            $updateNeeded = true;
+        }
+
+        return $updateNeeded;
+    }
+
+    /**
+     * @return string[] All new fields and tables must exist
+     */
+    public function getPrerequisites(): array
+    {
+        return [
+            DatabaseUpdatedPrerequisite::class
+        ];
+    }
+
+    /**
+     * Performs the accordant updates.
+     *
+     * @return bool Whether everything went smoothly or not
+     * @throws DBALException
+     */
+    public function executeUpdate(): bool
+    {
+        $this->migrateNewsImagesToFal();
+
+        return true;
+    }
+
+    /**
+     *
+     * @throws DBALException
+     */
+    protected function migrateNewsImagesToFal()
+    {
+        $this->connectionPool = GeneralUtility::makeInstance(ConnectionPool::class);
+
+        /** @var $conn Connection */
+        $conn = $this->connectionPool->getConnectionForTable($this->table);
+
+        $notMigratedNewsRecords = $conn->executeQuery('
+                SELECT n.uid, n.pid, n.image, r.uid as relId
+                FROM tt_news n 
+                LEFT JOIN sys_file_reference r 
+                    ON r.uid_foreign = n.uid AND r.tablenames = \'tt_news\' AND r.fieldname = \'image\'
+                
+                WHERE n.image != \'\'
+                  AND n.deleted = 0
+                  AND r.uid IS NULL
+                 '
+        )->fetchAll();
+
+        if (!empty($notMigratedNewsRecords)) {
+
+            $sourceFolder = 'uploads/pics/';
+            $targetFolder = 'fileadmin/migratedNewsAssets/Images';
+            $pathSite = Environment::getPublicPath() . '/';
+            if (!is_dir($pathSite . $targetFolder)) {
+                mkdir($pathSite . $targetFolder, 0777, true);
+            }
+            /** @var ResourceFactory $resourceFactory */
+            $resourceFactory = GeneralUtility::makeInstance(ResourceFactory::class);
+            /** @var Folder $folder */
+            $folder = $resourceFactory->retrieveFileOrFolderObject($pathSite . $targetFolder);
+
+            foreach ($notMigratedNewsRecords as $newsRecord) {
+
+                $images = GeneralUtility::trimExplode(',', $newsRecord['image']);
+                if (!empty($images)) {
+                    $existingImagesCount = 0;
+                    foreach ($images as $image) {
+                        if (file_exists($pathSite . $sourceFolder . $image)) {
+                            $file = $folder->addFile($pathSite . $sourceFolder . $image, null, DuplicationBehavior::REPLACE);
+                            if ($file instanceof File) {
+                                $this->insertFileReference($file, $newsRecord);
+                            }
+                            $existingImagesCount++;
+                        }
+                    }
+                    if ($existingImagesCount > 0) {
+                        $conn->update($this->table,
+                            ['image' => $existingImagesCount],
+                            ['uid' => $newsRecord['uid']]
+                        );
+                    }
+                }
+            }
+        }
+
+//        file_put_contents(Environment::getPublicPath() . '/' . 'typo3temp/log.log',
+//            date('YmdHis', time()) . PHP_EOL . __FILE__ . '::' . __LINE__ . PHP_EOL . print_r(array(
+//                __METHOD__,
+//
+//                $notMigratedNewsRecords
+//            ), true) . PHP_EOL, FILE_APPEND);
+
+    }
+
+    /**
+     * @param File  $file
+     * @param array $newsRecord
+     */
+    protected function insertFileReference($file, $newsRecord)
+    {
+        $refTable = 'sys_file_reference';
+        if (!empty($file)) {
+            $insertFileRelFields = [
+                'pid' => $newsRecord['pid'],
+                'tablenames' => $this->table,
+                'fieldname' => 'image',
+                'table_local' => 'sys_file',
+                'uid_local' => $file->getUid(),
+                'uid_foreign' => $newsRecord['uid']
+            ];
+
+            /** @var $conn Connection */
+            $conn = $this->connectionPool->getConnectionForTable($refTable);
+            $conn->insert($refTable, $insertFileRelFields);
+            $lastInsertId = $conn->lastInsertId($refTable);
+
+            /** @var ReferenceIndex $refIndexObj */
+            $refIndexObj = GeneralUtility::makeInstance(ReferenceIndex::class);
+            $refIndexObj->enableRuntimeCache();
+            $refIndexObj->updateRefIndexTable($refTable, $lastInsertId);
+        }
+    }
+
+
+    /**
+     * Check if there are record within "pages" database table with an empty "slug" field.
+     *
+     * @return bool
+     * @throws InvalidArgumentException
+     * @throws DBALException
+     */
+    protected function checkIfWizardIsRequired(): bool
+    {
+        /** @var $conn Connection */
+        $conn = $this->connectionPool->getConnectionForTable($this->table);
+
+        $numberOfEntries = $conn->executeQuery('
+                SELECT count(*) as c
+                FROM tt_news n 
+                LEFT JOIN sys_file_reference r 
+                    ON r.uid_foreign = n.uid AND r.tablenames = \'tt_news\' AND r.fieldname = \'image\'
+                
+                WHERE n.image != \'\'
+                  AND n.deleted = 0
+                  AND r.uid IS NULL
+                 '
+        )->fetch();
+
+        return $numberOfEntries['c'] > 0;
+    }
+}
