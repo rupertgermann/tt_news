8ffc50d38d7757c421a368adece1e8285dce6a60
Patrick Lenk <dev@lencen.de>
2020-10-22 16:28:37 +0200

    [!!!][TASK] Streamline template

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,8 +1,7 @@
 <?php
 defined('TYPO3_MODE') or die();
 // Add static extension templates
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tt_news', 'Configuration/TypoScript/ts_new/', 'News settings');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tt_news', 'Configuration/TypoScript/css/', 'News CSS-styles');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tt_news', 'Configuration/TypoScript/rss_feed/',
-    'News feeds (RSS,RDF,ATOM)');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tt_news', 'Configuration/TypoScript/', 'News settings');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tt_news', 'Configuration/TypoScript/Css/', 'News CSS-styles');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tt_news', 'Configuration/TypoScript/Rss/', 'News feeds (RSS,RDF,ATOM)');
 
