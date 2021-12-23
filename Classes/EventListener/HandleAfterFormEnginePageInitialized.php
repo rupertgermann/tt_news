4e39524782366d8cedaff4978737f0192bb395c4
Rupert Germann <rupert.germann@pick2.de>
2021-12-23 18:26:54 +0100

    [TASK] optimized handling of allowed/disallowed categories when saving news records

---
+++ Classes/EventListener/HandleAfterFormEnginePageInitialized.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace RG\TtNews\EventListener;
+
+
+use RG\TtNews\Database\Database;
+use RG\TtNews\Utility\Div;
+use TYPO3\CMS\Backend\Controller\Event\AfterFormEnginePageInitializedEvent;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
+use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Messaging\FlashMessage;
+use TYPO3\CMS\Core\Messaging\FlashMessageService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class HandleAfterFormEnginePageInitialized
+{
+    /**
+     * @param AfterFormEnginePageInitializedEvent $event
+     *
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function __invoke(AfterFormEnginePageInitializedEvent $event): void
+    {
+        $request = $event->getRequest();
+        $queryParams = $request->getQueryParams();
+
+        if (!$this->getBeUser()->isAdmin()) {
+            foreach ($queryParams['edit'] ?? [] as $table => $conf) {
+                if ($table == 'tt_news') {
+                    foreach ($conf as $id => $command) {
+                        if ($command === 'edit') {
+                            // get categories from the (untranslated) record in db
+                            $res = Database::getInstance()->exec_SELECT_mm_query(
+                                'tt_news_cat.uid, tt_news_cat.title',
+                                'tt_news',
+                                'tt_news_cat_mm',
+                                'tt_news_cat',
+                                ' AND tt_news_cat.deleted=0 AND tt_news_cat_mm.uid_local=' . (int)$id . BackendUtility::BEenableFields(
+                                    'tt_news_cat'
+                                )
+                            );
+                            $categories = [];
+                            while (($row = Database::getInstance()->sql_fetch_assoc($res))) {
+                                $categories[$row['uid']] = $row['title'];
+                            }
+
+                            $notAllowedItems = [];
+
+                            $allowedItems = $this->getBeUser()->getTSConfig()['tt_newsPerms.']['tt_news_cat.']['allowedItems'];
+                            $allowedItems = $allowedItems ? GeneralUtility::intExplode(
+                                ',',
+                                $allowedItems
+                            ) : Div::getAllowedTreeIDs();
+
+                            foreach ($categories as $categoryId => $categoryTitle) {
+                                if (!in_array($categoryId, $allowedItems)) {
+                                    $notAllowedItems[] = $categoryTitle . ' (id=' . $categoryId . ')';
+                                }
+                            }
+
+                            if (!empty($notAllowedItems)) {
+                                $notAllowedItemsMessage = $this->getLanguageService()
+                                    ->sL('LLL:EXT:tt_news/Resources/Private/Language/locallang_tca.xml:tt_news.notAllowedCategoryWarning') . implode(
+                                        ', ',
+                                        $notAllowedItems
+                                    );
+
+                                $flashMessage = GeneralUtility::makeInstance(
+                                    FlashMessage::class,
+                                    $notAllowedItemsMessage,
+                                    '',
+                                    FlashMessage::WARNING
+                                );
+                                $flashMessageService = GeneralUtility::makeInstance(FlashMessageService::class);
+                                $defaultFlashMessageQueue = $flashMessageService->getMessageQueueByIdentifier();
+                                $defaultFlashMessageQueue->enqueue($flashMessage);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    /**
+     * @return BackendUserAuthentication
+     */
+    protected function getBeUser()
+    {
+        return $GLOBALS['BE_USER'];
+    }
+
+    /**
+     * Returns LanguageService
+     *
+     * @return LanguageService
+     */
+    protected function getLanguageService()
+    {
+        return $GLOBALS['LANG'];
+    }
+}
--- Classes/Tree/TableConfiguration/NewsDatabaseTreeDataProvider.php
+++ Classes/Tree/TableConfiguration/NewsDatabaseTreeDataProvider.php
@@ -4,9 +4,14 @@ namespace RG\TtNews\Tree\TableConfiguration;
 
 use RG\TtNews\Database\Database;
 use RG\TtNews\Utility\Div;
+use TYPO3\CMS\Backend\Tree\SortedTreeNodeCollection;
 use TYPO3\CMS\Backend\Tree\TreeNode;
 use TYPO3\CMS\Backend\Tree\TreeNodeCollection;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
+use TYPO3\CMS\Core\Imaging\Icon;
+use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Tree\TableConfiguration\DatabaseTreeNode;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Tree\TableConfiguration\DatabaseTreeDataProvider;
 
@@ -28,7 +33,6 @@ class NewsDatabaseTreeDataProvider extends DatabaseTreeDataProvider
     protected function getChildrenOf(TreeNode $node, $level): ?TreeNodeCollection
     {
         $allowedItems = $this->getBeUser()->getTSConfig()['tt_newsPerms.']['tt_news_cat.']['allowedItems'];
-
         $allowedItems = $allowedItems ? GeneralUtility::intExplode(',', $allowedItems) : Div::getAllowedTreeIDs();
 
         $storage = null;
@@ -65,7 +69,7 @@ class NewsDatabaseTreeDataProvider extends DatabaseTreeDataProvider
             $node = GeneralUtility::makeInstance(TreeNode::class);
 
             if (!in_array($child, $allowedItems)) {
-                continue;
+                $this->setItemUnselectableList(array_merge($this->getItemUnselectableList() ?? [], [$child]));
             }
 
             $node->setId($child);
@@ -82,6 +86,63 @@ class NewsDatabaseTreeDataProvider extends DatabaseTreeDataProvider
         }
 
         return $storage;
+    }
+
+
+    /**
+     * Builds a complete node including childs
+     *
+     * @param \TYPO3\CMS\Backend\Tree\TreeNode $basicNode
+     * @param \TYPO3\CMS\Core\Tree\TableConfiguration\DatabaseTreeNode|null $parent
+     * @param int $level
+     * @return \TYPO3\CMS\Core\Tree\TableConfiguration\DatabaseTreeNode Node object
+     */
+    protected function buildRepresentationForNode(TreeNode $basicNode, DatabaseTreeNode $parent = null, $level = 0)
+    {
+        /** @var \TYPO3\CMS\Core\Tree\TableConfiguration\DatabaseTreeNode $node */
+        $node = GeneralUtility::makeInstance(DatabaseTreeNode::class);
+        $row = [];
+        if ($basicNode->getId() == 0) {
+            $node->setSelected(false);
+            $node->setExpanded(true);
+            $node->setLabel($this->getLanguageService()->sL($GLOBALS['TCA'][$this->tableName]['ctrl']['title']));
+        } else {
+            $row = BackendUtility::getRecordWSOL($this->tableName, (int)$basicNode->getId(), '*', '', false);
+            $node->setLabel(BackendUtility::getRecordTitle($this->tableName, $row) ?: $basicNode->getId());
+            $node->setSelected(GeneralUtility::inList($this->getSelectedList(), $basicNode->getId()));
+            $node->setExpanded($this->isExpanded($basicNode));
+        }
+        $node->setId($basicNode->getId());
+        $node->setSelectable(!GeneralUtility::inList($this->getNonSelectableLevelList(), (string)$level) && !in_array($basicNode->getId(), $this->getItemUnselectableList()));
+        $node->setSortValue($this->nodeSortValues[$basicNode->getId()]);
+
+        /** @var IconFactory $iconFactory */
+        $iconFactory = GeneralUtility::makeInstance(IconFactory::class);
+
+        if (in_array($basicNode->getId(), $this->getItemUnselectableList())) {
+            $iconIdentifier = $iconFactory->mapRecordTypeToIconIdentifier($this->tableName, $row);
+            $node->setIcon($iconFactory->getIcon($iconIdentifier, Icon::SIZE_SMALL, 'overlay-readonly'));
+            if (GeneralUtility::inList($this->getSelectedList(), $basicNode->getId())) {
+                $node->setLabel('[X] ' . $node->getLabel());
+            }
+        } else {
+            $node->setIcon($iconFactory->getIconForRecord($this->tableName, $row, Icon::SIZE_SMALL));
+        }
+
+        $node->setParentNode($parent);
+        if ($basicNode->hasChildNodes()) {
+            $node->setHasChildren(true);
+            /** @var \TYPO3\CMS\Backend\Tree\SortedTreeNodeCollection $childNodes */
+            $childNodes = GeneralUtility::makeInstance(SortedTreeNodeCollection::class);
+            $tempNodes = [];
+            foreach ($basicNode->getChildNodes() as $child) {
+                $tempNodes[] = $this->buildRepresentationForNode($child, $node, $level + 1);
+            }
+            $childNodes->exchangeArray($tempNodes);
+            $childNodes->asort();
+            $node->setChildNodes($childNodes);
+        }
+        return $node;
     }
 
     /**
