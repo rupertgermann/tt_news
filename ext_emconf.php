d784a12222d222f77ef793c668a270a3e0560cd0
Rupert Germann <rupert.germann@pick2.de>
2022-10-13 09:18:21 +0200

    [TASK] set version constraints, drop support for TYPO3 v9

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF['tt_news'] = [
     'author_company' => 'www.pick2.de',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-11.5.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
