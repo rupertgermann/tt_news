f2d7e8bfb4c237dbc80adfcbacfd75cbedecb98b
Patrick Lenk <dev@avatara.de>
2024-01-17 16:15:04 +0100

    [FEATURE] add dev tools
    - ddev with minimal typo3 setup
    - typo3-rector with config for typo3 11 and php 8.1
    - php-cs-fixer with config from typo3/coding-standards
    - editorconfig
    - update gitignore
    
    This enables faster development of a potential version for newer typo3 versions.

---
+++ Build/rector.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+// Src: https://github.com/sabbelasichon/typo3-rector/blob/v1.6.0/templates/rector.php.dist
+
+use Rector\Config\RectorConfig;
+use Rector\Core\ValueObject\PhpVersion;
+use Rector\Set\ValueObject\LevelSetList;
+use Ssch\TYPO3Rector\Set\Typo3LevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_81,
+        Typo3LevelSetList::UP_TO_TYPO3_11,
+    ]);
+
+    // Define your target version which you want to support
+    $rectorConfig->phpVersion(PhpVersion::PHP_81);
+
+    // If you only want to process one/some TYPO3 extension(s), you can specify its path(s) here.
+    // If you use the option --config change __DIR__ to getcwd()
+    $rectorConfig->paths([
+        __DIR__ . '/..',
+    ]);
+
+    // When you use rector, there are rules that require some more actions like creating UpgradeWizards for outdated TCA types.
+    // To fully support you, we added some warnings. So watch out for them.
+
+    // If you use importNames(), you should consider excluding some TYPO3 files.
+    $rectorConfig->skip([
+        __DIR__ . '/../Configuration/TypoScript/*.txt', // exclude typescript which would result in false positive processing
+    ]);
+};
