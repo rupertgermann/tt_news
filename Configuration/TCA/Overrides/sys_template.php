0ad1620ecd99681275861a2573c29c3431c02f4f
Patrick Lenk <dev@avatara.de>
2024-01-23 11:42:35 +0100

    [TASK] cleanup

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,6 +3,7 @@
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') or die();
+
 // Add static extension templates
 ExtensionManagementUtility::addStaticFile('tt_news', 'Configuration/TypoScript', 'News Settings');
 ExtensionManagementUtility::addStaticFile('tt_news', 'Configuration/TypoScript/Styles', 'News Styles');
