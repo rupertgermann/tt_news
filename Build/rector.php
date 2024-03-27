cab9c7495ad54fd2571e58634d26a74137b6dcb1
Patrick Lenk <dev@avatara.de>
2024-03-27 11:11:13 +0100

    [TASK] update rector config

--- Build/rector.php
+++ Build/rector.php
@@ -5,20 +5,24 @@ declare(strict_types=1);
 // Src: https://github.com/sabbelasichon/typo3-rector/blob/main/templates/rector.php.dist
 
 use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 use Rector\ValueObject\PhpVersion;
 use Ssch\TYPO3Rector\Set\Typo3LevelSetList;
+use Ssch\TYPO3Rector\Set\Typo3SetList;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/..',
     ])
-    ->withPhpSets(
-        true
-    )
+    ->withPhpVersion(PhpVersion::PHP_81)
     ->withSets([
+        Typo3SetList::CODE_QUALITY,
+        Typo3SetList::GENERAL,
         Typo3LevelSetList::UP_TO_TYPO3_12,
     ])
-    ->withPhpVersion(PhpVersion::PHP_81)
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+    ])
     ->withSkip([
         __DIR__ . '/../.Build/*',
         __DIR__ . '/../.ddev/*',
