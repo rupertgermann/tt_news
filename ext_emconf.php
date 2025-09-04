db2bfdd477e22dbf21f951f924317273c3dc6273
Rupert Germann <rupert.germann@pick2.de>
2025-09-04 13:05:26 +0200

    feat: start v13 development

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF['tt_news'] = [
     'title' => 'News',
     'description' => 'Website news with front page teasers and article handling inside.',
     'category' => 'plugin',
-    'version' => '12.0.1',
+    'version' => '13.0.0',
     'state' => 'alpha',
     'author' => 'Rupert Germann [pick2webServices]',
     'author_email' => 'rg@pick2.de',
     'author_company' => 'www.pick2.de',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.10-12.4.99',
+            'typo3' => '12.4.10-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
